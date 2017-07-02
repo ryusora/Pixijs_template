969366fec5d016e74c0d90fb3546262f4fd53474
RyuSora <nam.vtrung@gmail.com>
2017-07-03 00:16:53 +0700

    * Fix issue with Question Items

--- js/Games/ItemsManager.js
+++ js/Games/ItemsManager.js
@@ -313,7 +313,7 @@ ItemsManager.prototype.CheckCollision = function(box)
 	{
 		if(this.items_actived[idx].CheckCollision(box))
 		{
-			//if(!this.items_actived[idx].isLuckyItem)
+			if(!this.items_actived[idx].isQuestionItem)
 			{
 				if(this.items_actived[idx].score < 0)
 				{
